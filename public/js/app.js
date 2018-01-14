54ccccfa095e90daa872e7af28da99485503dd5b
beth92 <bethtownsend92@gmail.com>
2018-01-14 16:08:14 -0500

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,32 @@
+/* jshint esversion: 6 */
+
+// import angular from angular; 
+
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope, $interval) {
+  $scope.mins = 0;
+  $scope.secs = 0;
+  $scope.running = false;
+  var timer;
+  $scope.limit = 5.0;
+  $scope.units = 'min';
+
+  $scope.startTimer = function () {
+    $scope.running = true;
+    var limitSecs = $scope.units === 'sec' ? $scope.limit : $scope.limit * 60;
+    var startTime = (new Date()).getTime();
+    timer = $interval(function(){
+      let newTime = (new Date().getTime() - startTime)/1000.0;
+      $scope.mins = (newTime / 60);
+      $scope.secs = newTime % 60;
+      if (newTime >= limitSecs){
+        $scope.stopTimer();
+      }
+    },10);
+  };
+
+  $scope.stopTimer = function() {
+    $interval.cancel(timer);
+    $scope.running = false;
+  };
+});
